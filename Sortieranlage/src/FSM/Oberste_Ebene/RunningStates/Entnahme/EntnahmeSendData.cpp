927d7e7ae9282e1a7196ef71eac09af8426475bc
acq713 <noel.chemier@haw-hamburg.de>
2023-01-19 00:08:39 +0100

    MQTT tests

--- Sortieranlage/src/FSM/Oberste_Ebene/RunningStates/Entnahme/EntnahmeSendData.cpp
+++ Sortieranlage/src/FSM/Oberste_Ebene/RunningStates/Entnahme/EntnahmeSendData.cpp
@@ -18,8 +18,10 @@ void EntnahmeSendData::entry() {
 
 void EntnahmeSendData::exit() {
 
-//	data->createJSON(data->Q1.front()->id, data->Q1.front()->tup, data->Q1.front()->heightSA1mean,
-//				data->Q1.front()->heightSA2);
+	data->createJSON(data->Q1.front()->id, data->Q1.front()->tup, data->Q1.front()->heightSA1mean,
+				data->Q1.front()->heightSA2);
+	data->sendJSON();
+
 	data->Q1.pop();
 
 	send_event(PSMG_SW_BAND_FREI);
